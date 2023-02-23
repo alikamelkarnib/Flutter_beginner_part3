5a2a6c5b6a0a1f20c383373bccc99f7dfd73603c
== <=>
2023-02-23 17:05:02 +0200

    Flutter-lesson23 Maps and Routing

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,1 +1,26 @@
 import 'package:flutter/material.dart';
+
+
+
+class Chooselocation extends StatefulWidget{
+@override
+State<Chooselocation> createState() => _ChooselocationState();
+}
+
+class _ChooselocationState extends State<Chooselocation> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Text('choose location screen'),
+
+    );
+  }}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,14 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body:Text('home screen'),
+     body: SafeArea(
+         child: Column(
+           children: <Widget> [
+             TextButton.icon(onPressed: (){}, icon: Icon(Icons.edit_location),
+             label:Text('Edit Location'),
+             )
+           ],
+         )),
 
 
     );
