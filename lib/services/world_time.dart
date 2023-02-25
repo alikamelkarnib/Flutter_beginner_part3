26a9364405635aac255fa522ade205142a8ef4ba
== <=>
2023-02-25 12:29:40 +0200

    Flutter-lesson 29 Error Handling

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -9,20 +9,29 @@ class WorldTime{
   String flag;
   String url;
   WorldTime({required this.location,required this.flag,required this.url});
+
+
   Future<void> getTimeData() async{
-
-    Response response =await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
-    Map data=jsonDecode(response.body);
+    try{
+  Response response =await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+  Map data=jsonDecode(response.body);
 //print (data);
-    String datatime =data['datetime'];
-    String offest=data['utc_offset'].substring(1,3);
-    //print(datatime) ;
-    print(offest);
-    DateTime now=DateTime.parse(datatime);
-    now= now.add(Duration(hours: int.parse(offest)));
-   time=now.toString();
+  String datatime =data['datetime'];
+  String offest=data['utc_offset'].substring(1,3);
+  //print(datatime) ;
+  print(offest);
+  DateTime now=DateTime.parse(datatime);
+  now= now.add(Duration(hours: int.parse(offest)));
+  time=now.toString();
 
 
 
+
+  }
+ catch(e){
+   print('catch error :$e');
+   time='non data';
+
+  }
   }
 }
