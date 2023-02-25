043c02ec9759dacc488b65adabfa7e516d247c9b
== <=>
2023-02-25 09:24:45 +0200

    Flutter-lesson 28 world time class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,39 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 class Loading extends StatefulWidget{
   @override
   State<Loading> createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
-  void getTimeData() async{
-
-Response response =await get(Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Beirut'));
-Map data=jsonDecode(response.body);
-//print (data);
-  String datatime =data['datetime'];
-  String offest=data['utc_offset'].substring(1,3);
-  //print(datatime) ;
-    print(offest);
-    DateTime now=DateTime.parse(datatime);
-   now= now.add(Duration(hours: int.parse(offest)));
-    print(now);
-
-
-  }
+  String time='loading';
+void   setupwordtime() async{
+  WorldTime instance =WorldTime(location :'Beirut',flag:'leb.png',url:'Asia/Beirut');
+  await instance.getTimeData();
+  print(instance.time);
+  setState(() {
+    time=instance.time;
+  });
+}
   @override
   void initState() {
 
     super.initState();
-    getTimeData();
+setupwordtime();
 
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child:Text(time),
+      ),
 
 
     );
---
+++ lib/services/world_time.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime{
+
+
+  String location;
+  String time='';
+  String flag;
+  String url;
+  WorldTime({required this.location,required this.flag,required this.url});
+  Future<void> getTimeData() async{
+
+    Response response =await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+    Map data=jsonDecode(response.body);
+//print (data);
+    String datatime =data['datetime'];
+    String offest=data['utc_offset'].substring(1,3);
+    //print(datatime) ;
+    print(offest);
+    DateTime now=DateTime.parse(datatime);
+    now= now.add(Duration(hours: int.parse(offest)));
+   time=now.toString();
+
+
+
+  }
+}
