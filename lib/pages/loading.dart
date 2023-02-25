63d72b1d360ccdfbf4ab469f38a76090673093be
== <=>
2023-02-25 16:55:02 +0200

    Flutter-lesson 32 loaders/spinners

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 class Loading extends StatefulWidget{
   @override
   State<Loading> createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+  set spinkit(SpinKitWave spinkit) {}
+
+ 
+
 
 void   setupwordtime() async{
   WorldTime instance =WorldTime(location :'Beirut',flag:'leb.png',url:'Asia/Beirut');
   await instance.getTimeData();
 Navigator.pushReplacementNamed(context, '/home',arguments: {
-  'location':instance.location,'flag':instance.flag,'time':instance.time,
+ 'location':instance.location,'flag':instance.flag,'time':instance.time,
 });
 
 }
@@ -26,9 +31,12 @@ setupwordtime();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child:Text('loading'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child:spinkit = SpinKitWave(
+          color: Colors.white,
+          size: 80.0,
+        ),
       ),
 
 
